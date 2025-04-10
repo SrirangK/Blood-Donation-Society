c81269d91eec81e7c2d43a0b3d6f25290a19060f
Srirang Kalantri <srirang.kalantri@manastik.com>
2025-04-10 18:06:43 +0530

    db change

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://hp15series:hp15series@cluster0.kscvv9n.mongodb.net/?retryWrites=true&w=majority').then(() => {
+mongoose.connect('mongodb+srv://shrikal19:0srirang%40@cluster0.6u9rrl5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => {
     console.log(`DB connection successful`);
 }).catch((e) => {
     console.log(`connection failed` , e.message);
